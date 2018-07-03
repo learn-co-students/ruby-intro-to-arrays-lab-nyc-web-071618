aa0bc69d4b5a9b7a23261f4b4ea8df274fb2c3c6
Jessica Long <jessicalong627@gmail.com>
2018-07-03 18:52:32 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+  instantiate_new_array = []
+  @my_new_array = instantiate_new_array
+end
+
+def array_with_two_elements
+  array_with_two_elements = [1, 3]
+  @my_two_array = array_with_two_elements
+end
+
+def first_element(array)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift[0]
+end
+
+def third_element(array)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift[2]
+end
+
+def last_element(array)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(first_country)
+  @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  @south_east_asia.first
+end
+
+def last_element_with_array_methods(last_country)
+  @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  @south_east_asia.last
+end
+
+def length_of_array(array)
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  @programming_languages.length
+end
